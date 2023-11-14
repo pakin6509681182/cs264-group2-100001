9a0df8f11364d20942d6c9415152df5904806b21
Natanan Phopradit <natanan.pho@dome.tu.ac.th>
2023-11-14 19:01:19 +0700

    add script for teacher and staff login mock test

--- subjectRequestForm/src/main/resources/static/js/loginScript.js
+++ subjectRequestForm/src/main/resources/static/js/loginScript.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.onload = function () {
     sessionStorage.clear();
 }
 
@@ -22,14 +22,55 @@ function login(user, pass) {
     loginData.PassWord = pass;
 
     var jsonData = JSON.stringify(loginData);
-        fetch('https://restapi.tu.ac.th/api/v1/auth/Ad/verify', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Application-Key': 'TU7908dee6d8b885c29f0457b4b709233032e25f50e6554a70f84832c60159d12e3b0bd6aa3c4332012bcf227261a0b51c'
-            },
-            body: jsonData
-        })
+    if (loginData.UserName == "teacher" && loginData.PassWord == "teacherpasswordtest") {
+        var userData = {
+            "status": true,
+            "message": "Success",
+            "type": "teacher",
+            "username": "teacher",
+            "displayname_th": "อาจารย์ ทดสอบ",
+            "displayname_en": "teacher  test",
+            "StatusWork": "1",
+            "StatusEmp": "ปกติ",
+            "email": "teachertest@tu.ac.th",
+            "department": "test",
+            "organization": "test"
+        }
+        for (let key in userData) {
+            sessionStorage.setItem(key, userData[key]);
+        }
+        resultElement.textContent = 'กำลังเข้าสู่ระบบ';
+        window.location = "main teacher.html";
+    }
+    else if (loginData.UserName == "employee" && loginData.PassWord == "employeepasswordtest") {
+        var userData = {
+            "status": true,
+            "message": "Success",
+            "type": "employee",
+            "username": "employee",
+            "displayname_th": "สตาฟ ทดสอบ",
+            "displayname_en": "employee  test",
+            "StatusWork": "1",
+            "StatusEmp": "ปกติ",
+            "email": "employeetest@tu.ac.th",
+            "department": "test",
+            "organization": "test"
+        }
+        for (let key in userData) {
+            sessionStorage.setItem(key, userData[key]);
+        }
+        resultElement.textContent = 'กำลังเข้าสู่ระบบ';
+        window.location = "main staff.html";
+    }
+
+    fetch('https://restapi.tu.ac.th/api/v1/auth/Ad/verify', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Application-Key': 'TU7908dee6d8b885c29f0457b4b709233032e25f50e6554a70f84832c60159d12e3b0bd6aa3c4332012bcf227261a0b51c'
+        },
+        body: jsonData
+    })
         .then(response => response.json())
         .then(data => {
             // Display a success message on the web page
